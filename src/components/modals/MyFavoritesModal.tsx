32eb848231f9eaac00ab06fd4f51484e35f62758
seoyoonyi <yiseoyoon@gmail.com>
2024-06-03 22:41:41 +0900

    Design: 'MyFavorites' 모달의 리스트 뷰 디자인 추가

--- src/components/modals/MyFavoritesModal.tsx
+++ src/components/modals/MyFavoritesModal.tsx
@@ -3,6 +3,9 @@ import React, { MouseEventHandler, useRef } from 'react';
 import Modal from './Modal';
 import { ModalType } from '../../types/modalTypes';
 import { MODAL_CONFIGS } from '../../configs/modalConfigs';
+import Frame from '../Frame';
+import Button from '../buttons/Button';
+import { truncateTitle } from '../../utils/utils';
 
 interface IMyFavoritesModalProps {
 	open: boolean;
@@ -15,6 +18,11 @@ interface IMyFavoritesModalProps {
 const MyFavoritesModal = ({ open, style, onClose, onMinimize, onOpen }: IMyFavoritesModalProps) => {
 	const MyFavoritesModalRef = useRef(null);
 	const { key } = MODAL_CONFIGS.myFavorites;
+	const items = Array.from({ length: 10 }, (_, index) => ({
+		title: `이브, 프시케 그리고 푸른 수염의 아내 ${index + 1}`,
+		subtitle: '서브타이틀',
+		id: index,
+	}));
 	return (
 		<Modal
 			className="absolute"
@@ -28,7 +36,35 @@ const MyFavoritesModal = ({ open, style, onClose, onMinimize, onOpen }: IMyFavor
 			style={style}
 			modalKey={key as ModalType}
 		>
-			<div className="w-[500px]"></div>
+			<div className="flex gap-1 ml-0.5 my-2">
+				<Button className="px-3 py-[1px] text-sm">Playlist</Button>
+				<Button className="px-3 py-[1px] text-sm">Mixtape</Button>
+			</div>
+			<Frame boxShadow="in" bg="white" className="overflow-x-auto max-h-96">
+				{items.map((item, index) => (
+					<div
+						key={index}
+						className="flex items-center justify-between p-2 border-b border-dashed xl:p-4 border-retroGray"
+					>
+						<div className="flex items-center mr-3 xl:mr-4">
+							<div className="flex items-center font-eng justify-center w-8 h-8 text-lg xl:text-xl font-bold text-center leading-[2rem] xl:leading-[2.5rem] mr-2 xl:mr-4">
+								<img src="/icon-bars.svg" alt="icon-bars" />
+							</div>
+							<div className="flex items-center space-x-2">
+								<div className="flex items-center justify-center w-[68px] h-[38px] overflow-hidden xl:w-20 xl:h-11 bg-retroGray"></div>
+
+								<div>
+									<h4 className="font-bold">{truncateTitle(item.title)}</h4>
+									<p className="text-xs xl:text-sm">{item.subtitle}</p>
+								</div>
+							</div>
+						</div>
+						<div>
+							<button className="underline text-retroBlue">Remove</button>
+						</div>
+					</div>
+				))}
+			</Frame>
 		</Modal>
 	);
 };
